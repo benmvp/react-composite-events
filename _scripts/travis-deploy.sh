34bebb91c5c4b659846c125d935d8545693024cf
Ben Ilegbodu <ben@benmvp.com>
2017-08-05 23:16:00 -0700

    ci: Fix broken NPM publish script
    The Travis deploy script failed: https://travis-ci.org/benmvp/react-composite-events/jobs/261462054. In an effort to debug I'm moving the script to `after_success` but in order to ensure that it doesn't run on all the Node instances, I'm disabling all but the latest.

--- _scripts/travis-deploy.sh
+++ _scripts/travis-deploy.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Globally install semantic-release package for releasing
-yarn global add semantic-release
-
 # 1. Update package.json version number based on commits
 # 2. Publish new version to NPM
 # 3. Generate changelog + release on Github
