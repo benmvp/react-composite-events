abddd371b8243d6292528ee05562b0d0db38b67a
Ben Ilegbodu <ben@benmvp.com>
2017-08-05 23:45:23 -0700

    ci: Remove deploy script
    With b6e4cd6b9cf14749a9f7e1fbc6e6662fc4e8f9f5 I tried to bring back using `semantic-release` in a deploy script, but it doesn't work for some reason. The `semantic-release` library uses Travis w/ multiple node versions and releases in `after_success` so it must work somehow!

--- _scripts/travis-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# 1. Update package.json version number based on commits
-# 2. Publish new version to NPM
-# 3. Generate changelog + release on Github
-semantic-release pre && npm publish && semantic-release post
