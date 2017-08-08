1319e35076431c943f442602f264ed79032f2761
Ben Ilegbodu <ben@benmvp.com>
2017-08-07 19:26:05 -0700

    build: Use Rollup to generate UMD bundles
    In addition to having lib formats (ESM, CJS & UMD), we should also have a bundled dist in UMD for those clients that basically can't do anything. The dist can be used directly in a `<script>` or used in a legacy module system like requireJS. There's a minified production bundle as well as an uniminified development bundle. Both have sourcemaps.
    
    Basically had to duplicate all of the configuration for both gulp & rollup, but with shared objects things remained DRY.
    
    Also excluded the `src/` directory

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,23 @@ const sourcemaps = require('gulp-sourcemaps')
 const replace = require('gulp-replace')
 const debug = require('gulp-debug')
 const util = require('gulp-util')
+
 const del = require('del')
+
+const {rollup} = require('rollup')
+const rollupBabel = require('rollup-plugin-babel')
+const rollupNodeResolve = require('rollup-plugin-node-resolve')
+const rollupCommonjs = require('rollup-plugin-commonjs')
+const rollupReplace = require('rollup-plugin-replace')
+const rollupUglify = require('rollup-plugin-uglify')
 
 const FORMAT_ESM = 'esm'
 const FORMAT_CJS = 'cjs'
 const FORMAT_UMD = 'umd'
+
+const MODULE_NAME = 'RCE'
+
+const SOURCE_ENTRY = 'src/index.js'
 
 const FILES_TO_BUILD = [
   // include all the JavaScript files in src/ directory
@@ -33,19 +45,30 @@ const UMD_TRANSFORM_PLUGIN = [
   {
     globals: {
       react: 'React',
-      index: 'RCE',
-      compose: 'RCE_compose',
-      generic: 'RCE_generic',
-      mouse: 'RCE_mouse',
-      key: 'RCE_key',
-      './compose': 'RCE_compose',
-      './generic': 'RCE_generic',
-      './mouse': 'RCE_mouse',
-      './key': 'RCE_key',
+      index: MODULE_NAME,
+      compose: `${MODULE_NAME}_compose`,
+      generic: `${MODULE_NAME}_generic`,
+      mouse: `${MODULE_NAME}_mouse`,
+      key: `${MODULE_NAME}_key`,
+      './compose': `${MODULE_NAME}_compose`,
+      './generic': `${MODULE_NAME}_generic`,
+      './mouse': `${MODULE_NAME}_mouse`,
+      './key': `${MODULE_NAME}_key`,
     },
     exactGlobals: true,
   },
 ]
+
+const _getBabelConfig = (format) => ({
+  babelrc: false,
+
+  presets: [format === FORMAT_ESM ? ESM_ENV_PRESET : 'env', 'stage-3', 'react'],
+  plugins: [
+    'transform-class-properties',
+    'external-helpers',
+    ...(format === FORMAT_UMD ? [UMD_TRANSFORM_PLUGIN] : []),
+  ],
+})
 
 const _getBabelStream = (format) =>
   gulp
@@ -54,45 +77,100 @@ const _getBabelStream = (format) =>
     // initialize the sourcemaps (used by UMD only)
     .pipe(format === FORMAT_UMD ? sourcemaps.init() : util.noop())
     // do the appropriate babel transpile (this is a copy from package.json)
-    .pipe(
-      babel({
-        babelrc: false,
+    .pipe(babel(_getBabelConfig(format)))
 
-        presets: [
-          format === FORMAT_ESM ? ESM_ENV_PRESET : 'env',
-          'stage-3',
-          'react',
-        ],
-        plugins: [
-          'transform-class-properties',
-          'external-helpers',
-          ...(format === FORMAT_UMD ? [UMD_TRANSFORM_PLUGIN] : []),
-        ],
-      })
-    )
+const _genRollupDist = (minify = false) =>
+  rollup({
+    entry: SOURCE_ENTRY,
 
-gulp.task('build:clean:cjs', () => del(['lib/cjs']))
-gulp.task('build:clean:esm', () => del(['lib/esm']))
-gulp.task('build:clean:umd', () => del(['lib/umd']))
+    // exclude React dependency from bundle
+    external: ['react'],
+
+    plugins: [
+      // Need to replace `process.env.NODE_ENV` in the bundle because most likely the place where this
+      // would be used doesn't support it. When minified we assume production, dev otherwise
+      rollupReplace({
+        'process.env.NODE_ENV': JSON.stringify(
+          minify ? 'production' : 'development'
+        ),
+      }),
+
+      // Locate modules using the Node resolution algorithm, for using third party modules in node_modules
+      rollupNodeResolve({
+        // use "module" field for ES6 module if possible
+        module: true,
+
+        // use (legacy) "jsnext:main" if possible
+        jsnext: true,
+
+        // use "main" field or index.js
+        main: true,
+      }),
+
+      // Convert CommonJS modules to ES6, so they can be included in a Rollup bundle
+      rollupCommonjs({
+        // Node modules are the ones we're trying to get it to understand
+        include: 'node_modules/**',
+      }),
+
+      // Seamless integration between Rollup and Babel
+      rollupBabel(
+        Object.assign(
+          {
+            // don't worry about transpiling node_modules when bundling
+            exclude: 'node_modules/**',
+
+            // don't place helpers at the top of the files, but point to reference contained external helpers
+            externalHelpers: true,
+          },
+          _getBabelConfig(FORMAT_ESM)
+        )
+      ),
+
+      // Minify the code if that option is specified
+      minify ? rollupUglify() : null,
+    ].filter(Boolean),
+  }).then((bundle) =>
+    bundle.write({
+      format: FORMAT_UMD,
+      dest: `dist/react-composite-events${minify ? '.min' : ''}.js`,
+
+      // The name to use for UMD bundle
+      moduleName: MODULE_NAME,
+
+      // Mapping of imported modules to globally accessible names for UMD bundle
+      globals: {
+        react: 'React',
+      },
+
+      sourceMap: true,
+    })
+  )
+
+gulp.task('build:clean:lib:cjs', () => del(['lib/cjs']))
+gulp.task('build:clean:lib:esm', () => del(['lib/esm']))
+gulp.task('build:clean:lib:umd', () => del(['lib/umd']))
+gulp.task('build:clean:dist', () => del(['dist/umd']))
 gulp.task('build:clean', [
-  'build:clean:cjs',
-  'build:clean:esm',
-  'build:clean:umd',
+  'build:clean:lib:cjs',
+  'build:clean:lib:esm',
+  'build:clean:lib:umd',
+  'build:clean:dist',
 ])
 
-gulp.task('build:cjs', ['build:clean:cjs'], () =>
+gulp.task('build:lib:cjs', ['build:clean:lib:cjs'], () =>
   _getBabelStream(FORMAT_CJS)
     .pipe(debug({title: 'Building CJS'}))
     .pipe(gulp.dest('lib/cjs'))
 )
 
-gulp.task('build:esm', ['build:clean:esm'], () =>
+gulp.task('build:lib:esm', ['build:clean:lib:esm'], () =>
   _getBabelStream(FORMAT_ESM)
     .pipe(debug({title: 'Building ESM:'}))
     .pipe(gulp.dest('lib/esm'))
 )
 
-gulp.task('build:umd', ['build:clean:umd'], () =>
+gulp.task('build:lib:umd', ['build:clean:lib:umd'], () =>
   _getBabelStream(FORMAT_UMD)
     // If you're using UMD, you probably don't have `process.env.NODE_ENV` so, we'll replace
     // it. If you're using the unimified UMD, you're probably in DEV
@@ -102,7 +180,7 @@ gulp.task('build:umd', ['build:clean:umd'], () =>
     .pipe(gulp.dest('lib/umd'))
 )
 
-gulp.task('build:umd:min', ['build:clean:umd'], () =>
+gulp.task('build:lib:umd:min', ['build:clean:lib:umd'], () =>
   _getBabelStream(FORMAT_UMD)
     // If you're using UMD, you probably don't have `process.env.NODE_ENV` so, we'll replace
     // it. If you're using the imified UMD, you're probably in production
@@ -115,6 +193,17 @@ gulp.task('build:umd:min', ['build:clean:umd'], () =>
     .pipe(gulp.dest('lib/umd'))
 )
 
-gulp.task('build', ['build:cjs', 'build:esm', 'build:umd', 'build:umd:min'])
+gulp.task('build:dist', ['build:clean:dist'], () => _genRollupDist())
+
+gulp.task('build:dist:min', ['build:clean:dist'], () => _genRollupDist(true))
+
+gulp.task('build', [
+  'build:lib:cjs',
+  'build:lib:esm',
+  'build:lib:umd',
+  'build:lib:umd:min',
+  'build:dist',
+  'build:dist:min',
+])
 
 gulp.task('default', ['build'])
