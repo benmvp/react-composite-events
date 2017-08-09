846300e3db43028368b8fd20316d299c315e2431
Ben Ilegbodu <ben@benmvp.com>
2017-08-08 22:35:39 -0700

    feat(API): Implement mouse-remain HOCs (#8)
    Implemented `withMouseRest`, `withMouseRemainOver` & `withMouseRemainOut` composite event HOCs within the mouse-related APIs. There are still other mouse-related APIs to implement

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export {default as compose} from './compose'
 export * from './generic'
+export * from './mouse'
---
+++ src/mouse.js
@@ -0,0 +1,25 @@
+import compose from './compose'
+
+export const withMouseRest = compose({
+  eventPropName: 'onMouseRest',
+  triggerEvent: ['onMouseOver', 'onMouseMove'],
+  defaultDuration: 500,
+  cancelEvent: ['onMouseOut', 'onMouseDown'],
+})
+
+export const withMouseRemainOver = compose({
+  eventPropName: 'onMouseRemainOver',
+  triggerEvent: ['onMouseOver', 'onMouseMove'],
+  defaultDuration: 500,
+  // including `onMouseDown` because clicking is basically a different action
+  cancelEvent: ['onMouseOut', 'onMouseDown'],
+  // as long as the mouse is over, moving around doesn't matter
+  shouldResetTimerOnRetrigger: false,
+})
+
+export const withMouseRemainOut = compose({
+  eventPropName: 'onMouseRemainOut',
+  triggerEvent: 'onMouseOut',
+  defaultDuration: 500,
+  cancelEvent: 'onMouseOver',
+})
