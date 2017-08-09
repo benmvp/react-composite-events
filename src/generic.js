7b1e44f3019ac4fe8e2a1748eb6906ca74a8b1eb
Ben Ilegbodu <ben@benmvp.com>
2017-08-08 20:03:04 -0700

    feat(withRemainReleased): Implement withRemainReleased

--- src/generic.js
+++ src/generic.js
@@ -6,3 +6,10 @@ export const withLongPress = compose({
   defaultDuration: 1250,
   cancelEvent: ['onMouseUp', 'onMouseOut', 'onPressOut'],
 })
+
+export const withRemainReleased = compose({
+  eventPropName: 'onRemainReleased',
+  triggerEvent: ['onMouseUp', 'onPressOut'],
+  defaultDuration: 500,
+  cancelEvent: ['onMouseDown', 'onPressIn'],
+})
--- src/generic.spec.js
+++ src/generic.spec.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {shallow} from 'enzyme'
-import {withLongPress} from './generic'
+import {withLongPress, withRemainReleased} from './generic'
 
 const Dummy = () => <div />
 
 jest.useFakeTimers()
 
-describe('withLongPress', () => {
+describe('`withLongPress`', () => {
   it('calls handler after mouse down & default 1250 ms', () => {
     const EnhancedAnchor = withLongPress()('a')
 
@@ -160,5 +160,144 @@ describe('withLongPress', () => {
     jest.runTimersToTime(800)
 
     expect(onLongPress).toHaveBeenCalledTimes(0)
+  })
+})
+
+describe('`withRemainReleased`', () => {
+  it('calls handler after mouse up & default 500 ms', () => {
+    const EnhancedAnchor = withRemainReleased()('a')
+
+    let onRemainReleased = jest.fn()
+    let wrapper = shallow(
+      <EnhancedAnchor
+        href="http://www.benmvp.com/"
+        onRemainReleased={onRemainReleased}
+      />
+    )
+    let anchorWrapper = wrapper.find('a')
+
+    expect(onRemainReleased).toHaveBeenCalledTimes(0)
+
+    // 1. simulate trigger
+    anchorWrapper.simulate('mouseup')
+
+    // 2. simulate going over time
+    jest.runTimersToTime(1250)
+
+    expect(onRemainReleased).toHaveBeenCalledTimes(1)
+  })
+
+  it('calls handler after press out & default 500 ms', () => {
+    const EnhancedDummy = withRemainReleased()(Dummy)
+
+    let onRemainReleased = jest.fn()
+    let wrapper = shallow(<EnhancedDummy onRemainReleased={onRemainReleased} />)
+    let dummyWrapper = wrapper.find(Dummy)
+
+    expect(onRemainReleased).toHaveBeenCalledTimes(0)
+
+    // 1. simulate trigger
+    dummyWrapper.prop('onPressOut')()
+
+    // 2. simulate going over time
+    jest.runTimersToTime(500)
+
+    expect(onRemainReleased).toHaveBeenCalledTimes(1)
+  })
+
+  it('calls handler after mouse up & specified duration', () => {
+    const EnhancedAnchor = withRemainReleased(450)('a')
+
+    let onRemainReleased = jest.fn()
+    let wrapper = shallow(
+      <EnhancedAnchor
+        href="http://www.benmvp.com/"
+        onRemainReleased-450={onRemainReleased}
+      />
+    )
+    let anchorWrapper = wrapper.find('a')
+
+    expect(onRemainReleased).toHaveBeenCalledTimes(0)
+
+    // 1. simulate trigger
+    anchorWrapper.simulate('mouseup')
+
+    // 2. simulate going over time
+    jest.runTimersToTime(450)
+
+    expect(onRemainReleased).toHaveBeenCalledTimes(1)
+  })
+
+  it('calls handler after press out & specified duration', () => {
+    const EnhancedDummy = withRemainReleased(876)(Dummy)
+
+    let onRemainReleased = jest.fn()
+    let wrapper = shallow(
+      <EnhancedDummy onRemainReleased-876={onRemainReleased} />
+    )
+    let dummyWrapper = wrapper.find(Dummy)
+
+    expect(onRemainReleased).toHaveBeenCalledTimes(0)
+
+    // 1. simulate trigger
+    dummyWrapper.prop('onPressOut')()
+
+    // 2. simulate going over time
+    jest.runTimersToTime(876)
+
+    expect(onRemainReleased).toHaveBeenCalledTimes(1)
+  })
+
+  it('does not call handler if mouse down happens before duration expires', () => {
+    const EnhancedAnchor = withRemainReleased()('a')
+
+    let onRemainReleased = jest.fn()
+    let wrapper = shallow(
+      <EnhancedAnchor
+        href="http://www.benmvp.com/"
+        onRemainReleased={onRemainReleased}
+      />
+    )
+    let anchorWrapper = wrapper.find('a')
+
+    expect(onRemainReleased).toHaveBeenCalledTimes(0)
+
+    // 1. simulate trigger
+    anchorWrapper.simulate('mouseup')
+
+    // 2. simulate some time passing
+    jest.runTimersToTime(450)
+
+    // 3. simulate cancel event
+    anchorWrapper.simulate('mousedown')
+
+    // 4. simulate going over time
+    jest.runTimersToTime(800)
+
+    expect(onRemainReleased).toHaveBeenCalledTimes(0)
+  })
+
+  it('does not call handler if press in happens before duration expires', () => {
+    const EnhancedDummy = withRemainReleased()(Dummy)
+
+    let onRemainReleased = jest.fn()
+    let wrapper = shallow(<EnhancedDummy onRemainReleased={onRemainReleased} />)
+    let dummyWrapper = wrapper.find(Dummy)
+
+    expect(onRemainReleased).toHaveBeenCalledTimes(0)
+
+    // 1. simulate trigger
+    dummyWrapper.prop('onPressOut')()
+
+    // 2. simulate some time passing
+    jest.runTimersToTime(450)
+
+    // 3. simulate cancel event
+    dummyWrapper.prop('onPressIn')()
+
+    // 4. simulate going over time
+    jest.runTimersToTime(800)
+
+    expect(onRemainReleased).toHaveBeenCalledTimes(0)
   })
 })
